44a2aa20e7d25a778b461e8766567831d5efe78d
rkuper <reese.kuper@gmail.com>
2020-08-27 00:36:05 -0500

    Day 8 - Finished education and experience

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function on(overlay_id, underlay_id, underlay_classes) {
-	document.getElementById(overlay_id).style.opacity = "0.95";
+function on(overlay_id, underlay_id, underlay_classes, button) {
+	document.getElementById(overlay_id).style.opacity = "1";
+	document.getElementById(button).style.opacity = "1";
 	var x = document.getElementById(underlay_id).getElementsByClassName(underlay_classes);
-  for (var i = 0; i < x.length; i++)
-    x[i].style.filter = "blur(15px)";
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.filter = "blur(5px)";
+    x[i].style.opacity = "0.35";
+	}
 }
 
-function off(overlay_id, underlay_id, underlay_classes) {
+function off(overlay_id, underlay_id, underlay_classes, button) {
 	document.getElementById(overlay_id).style.opacity = "0";
+	document.getElementById(button).style.opacity = "0.6";
 	var x = document.getElementById(underlay_id).getElementsByClassName(underlay_classes);
-  for (var i = 0; i < x.length; i++)
+  for (var i = 0; i < x.length; i++) {
     x[i].style.filter = "blur(0px)";
+    x[i].style.opacity = "1";
+	}
 }
